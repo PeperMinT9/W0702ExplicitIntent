b82abb1ed28a97105204abf2e05bb1de7f86451f
PeperMint9 <lobetion@gmail.com>
2022-10-19 17:22:22 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20170392/example/w0702explicitintent/MainActivity.kt
@@ -0,0 +1,33 @@
+package kr.ac.kumoh.s20170392.example.w0702explicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20170392.example.w0702explicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnExplicitIntent.setOnClickListener {
+            val str = binding.editText.text
+            val uri = Uri.parse(this, Expl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnYoutube.setOnClickListener {
+            val str = binding.editText.text
+            val uri = Uri.parse("https://www.youtube.com/results?search_query=" + str)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+
+    }
+}
